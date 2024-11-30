36d344f0925a591403d9cafb5f2463f11d899c10
HillTrigger <mslava32@gmail.com>
2024-11-30 17:43:59 +0600

    *

--- components/todo/model/handleSubmitInput.js
+++ components/todo/model/handleSubmitInput.js
@@ -2,6 +2,7 @@ import { ACTIONS } from "../constans";
 
 export function handleSubmitInput({ e, dispatch, setTextInput }) {
   e.preventDefault();
+  if (!e.target.firstElementChild.value) return;
   dispatch({
     type: ACTIONS.ADD_TODO,
     payload: { name: e.target.firstElementChild.value },
